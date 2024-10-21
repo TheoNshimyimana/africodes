f2c313f92c907c6a1eea2b7650d527f365410181
Theo <121424147+TheoNshimyimana@users.noreply.github.com>
2024-10-21 10:50:36 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 import './index.css'
-import LoaderBlank from './pages/LoaderBlank.jsx'
+// import LoaderBlank from './pages/LoaderBlank.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <LoaderBlank />
+    <App />
+{/*     <LoaderBlank /> */}
   </StrictMode>,
 )
